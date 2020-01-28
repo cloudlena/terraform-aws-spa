7af47677adabfe8a3a88983a700926c935379e1b
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2020-01-28 09:55:54 +0100

    Allow asset path patterns to be set for caching

--- cdn.tf
+++ cdn.tf
@@ -59,6 +59,30 @@ resource "aws_cloudfront_distribution" "main" {
     max_ttl                = 0
   }
 
+  dynamic "ordered_cache_behavior" {
+    for_each = var.asset_path_patterns
+
+    content {
+      path_pattern     = ordered_cache_behavior.value
+      allowed_methods  = ["GET", "HEAD"]
+      cached_methods   = ["GET", "HEAD"]
+      target_origin_id = "S3-${aws_s3_bucket.website.bucket}"
+
+      forwarded_values {
+        query_string = false
+        cookies {
+          forward = "none"
+        }
+      }
+
+      viewer_protocol_policy = "redirect-to-https"
+      compress               = true
+      min_ttl                = 0
+      default_ttl            = 31536000 # 1 year
+      max_ttl                = 31536000 # 1 year
+    }
+  }
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
