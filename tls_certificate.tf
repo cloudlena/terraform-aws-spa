ef2386bf0c4e9e03584c99c9e943a8668fbf50c1
Tobi Fuhrimann <tobias.fuhrimann@gmail.com>
2020-08-06 09:02:27 +0200

    Upgrade to Terraform AWS Provider 3

--- tls_certificate.tf
+++ tls_certificate.tf
@@ -16,17 +16,25 @@ resource "aws_acm_certificate" "main" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  name    = aws_acm_certificate.main.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.main.domain_validation_options[0].resource_record_type
-  zone_id = data.aws_route53_zone.main.id
-  records = [aws_acm_certificate.main.domain_validation_options[0].resource_record_value]
+  for_each = {
+    for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  name    = each.value.name
+  type    = each.value.type
+  zone_id = data.aws_route53_zone.main.zone_id
+  records = [each.value.record]
   ttl     = 60
 }
 
 resource "aws_acm_certificate_validation" "main" {
   provider                = aws.us_east
   certificate_arn         = aws_acm_certificate.main.arn
-  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 
   lifecycle {
     create_before_destroy = true
