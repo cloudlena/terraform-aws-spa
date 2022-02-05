0f72bc39540f303a8c73acae04fc873daeb7117e
Lena Fuhrimann <cloudlena@users.noreply.github.com>
2022-02-05 11:48:23 +0100

    Upgrade minimum_protocol_version to TLSv1.2_2021

--- cdn.tf
+++ cdn.tf
@@ -94,9 +94,9 @@ resource "aws_cloudfront_distribution" "main" {
   }
 
   viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate.main.arn
+    acm_certificate_arn      = aws_acm_certificate_validation.main.certificate_arn
     ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2019"
+    minimum_protocol_version = "TLSv1.2_2021"
   }
 
   tags = {
