86039ad316d3abd148f76f7ec1faa9626d3ef056
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2023-01-06 13:55:38 +0100

    Use bucket keys for S3 encryption

--- hosting.tf
+++ hosting.tf
@@ -19,6 +19,17 @@ resource "aws_s3_bucket_website_configuration" "website" {
   }
 }
 
+resource "aws_s3_bucket_server_side_encryption_configuration" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+    bucket_key_enabled = true
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "website" {
   bucket                  = aws_s3_bucket.website.id
   block_public_acls       = true
